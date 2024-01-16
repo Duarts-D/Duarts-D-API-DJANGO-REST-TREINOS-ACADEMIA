cce4b50fae15c67130d29c07e424e207bfcea8c8
Deivid <deividnaruto@hotmail.com>
2024-01-16 10:59:27 -0300

    Criado serializer para Cadastroviewset e criado teste unitario para serializador Usercadastro

---
+++ apps/usuarios/serializer.py
@@ -0,0 +1,52 @@
+from django.contrib.auth.models import User
+from rest_framework import serializers
+from django.utils.translation import gettext_lazy as _
+from collections import defaultdict
+
+
+class UserCadastroSerializer(serializers.ModelSerializer):
+    username = serializers.CharField(min_length=2,max_length=50,required=True)
+    repeat_password = serializers.CharField(
+        max_length=50,min_length=8,
+        label=_('Repetir Senha'),
+        write_only=True)
+    first_name = serializers.CharField(min_length=2,max_length=100,required=True)
+    last_name = serializers.CharField(min_length=2,max_length=100,required=True)
+    email = serializers.EmailField(required=True)
+    class Meta:
+        model = User
+        fields = ['username','first_name','last_name','email','password','repeat_password',]
+        extra_kwargs = {
+            'password': {'write_only': True },
+        }       
+        
+    def validate(self, data):
+        _my_errors = defaultdict(list)
+        email = data['email']
+        email_existe = User.objects.filter(email=email).exists()
+
+        senha = data['password']
+        senha_2 = data['repeat_password']
+
+        usuario = data['username']
+        usuario_existe = User.objects.filter(username=usuario).exists()
+
+
+        if usuario_existe:
+            _my_errors['username'].append(_('Um usuário com este nome de usuário já existe.'))
+        if email_existe:
+            _my_errors['email'].append(_('Este email ja foi utilizado'))
+        
+        if senha != senha_2:
+            _my_errors['password'].append(_('As senhas não coincidem'))
+            _my_errors['repeat_password'].append(_('As senhas não coincidem'))
+        if _my_errors:
+            raise serializers.ValidationError(_my_errors)
+        return data
+    
+    def create(self, validated_data):
+        validated_data.pop('repeat_password')
+        user = User.objects.create(**validated_data)
+        user.set_password(validated_data['password'])
+        user.save()
+        return user

a7b60173d33e66eb9496058e6e058e74f80101dc
Deivid <deividnaruto@hotmail.com>
2024-01-17 10:16:20 -0300

    Apps/academia criado model video e adicionando na admin inicialmente

--- apps/academia/admin.py
+++ apps/academia/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import VideoModel
 
 # Register your models here.
+@admin.register(VideoModel)
+class VideoAdmiModel(admin.ModelAdmin):
+    list_display = ['video_nome','video_id_youtube','video_id_didatico']
+    list_per_page = 10
--- apps/academia/models.py
+++ apps/academia/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class VideoModel(models.Model):
+    video_nome = models.CharField(max_length=255,verbose_name=_('Exercicio'))
+    video_id_youtube = models.CharField(max_length=255,verbose_name=_('Video id'))
+    video_url = models.CharField(max_length=100,blank=True,null=True,verbose_name=_("video_url"))
+    informacao = models.CharField(max_length=255,blank=True,null=True,verbose_name=_('Informacão'))
+    imagem = models.ImageField(upload_to='img/',blank=True,null=True,verbose_name=_('Imagem'))
+    video_id_didatico = models.CharField(max_length=50,blank=True,null=True,verbose_name=_('Video id didatico'))
+    def save(self,*args):
+        if len(self.video_id_youtube) > 2:
+            self.video_url = 'https://www.youtube.com/embed/' + self.video_id_youtube
+        return super().save(*args)
+
+    def __str__(self):
+        return self.video_nome
+    

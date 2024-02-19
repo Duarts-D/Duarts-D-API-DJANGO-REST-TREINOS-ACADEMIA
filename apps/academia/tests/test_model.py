5bf865c26710cc23679954c31cdc7ce3dff09444
Deivid <deividnaruto@hotmail.com>
2024-02-19 17:44:28 -0300

    Apps/academia/testmodel test mode unique unique_together

--- apps/academia/tests/test_model.py
+++ apps/academia/tests/test_model.py
@@ -3,7 +3,9 @@ from apps.academia.models import (VideoModel,GrupoMuscularModel,EquipamentoModel
                                   TreinoModel,TreinoVideosmodel)
 from django.contrib.auth.models import User
 from apps.usuarios.tests.test_cadastro_base import CadastroMixin
-from apps.academia.tests.test_geradores_base import Geradores_base_mixin
+from apps.academia.tests.utils_geradores_base import GeradoresBaseMixin
+from pytest import raises
+from django.db import IntegrityError
 
 class VideoModelTest(TestCase):
     def test_videomodel_esta_salvando_url_corretamente_com_os_parametro_inicial_do_youtube(self):
@@ -44,12 +46,30 @@ class TreinoModelTest(CadastroMixin,TestCase):
         self.assertEqual(respota.treino_nome,"Treino")
         self.assertEqual(respota.usuario, user)
         
+    def test_treino_model_usario_e_treino_nome_unicos_juntos(self):
+        usuario_1 = self.cadastro_usuario()
+        usuario_2 = self.cadastro_usuario(username='Usuario2')
 
-class TreinoVideosModelTest(Geradores_base_mixin,CadastroMixin,TestCase):
+        nome_treino = 'treino_teste'
+        
+        treino_1 = TreinoModel.objects.create(usuario=usuario_1,treino_nome=nome_treino)
+        treino_2 = TreinoModel.objects.create(usuario=usuario_2,treino_nome=nome_treino)
+        
+        self.assertEqual(treino_1.treino_nome,nome_treino)
+        self.assertEqual(treino_2.treino_nome,nome_treino)
+
+        with raises(IntegrityError) as msg_error:
+            TreinoModel.objects.create(usuario=usuario_1,treino_nome=nome_treino)
+        msg_esperada = 'UNIQUE constraint failed:'
+        self.assertIn(msg_esperada,str(msg_error.value))
+        
+
+
+class TreinoVideosModelTest(GeradoresBaseMixin,CadastroMixin,TestCase):
     def test_treino_videos_model_itegridade_de_forekey(self):
         user = self.cadastro_usuario(username='TesteTreino')
         treino = self.criar_treino()
-        videos = self.videos_multiplicos(5)
+        videos = self.criar_videos_multiplicos(5)
         treinovideos = TreinoVideosmodel.objects.create(
             usuario=user,
             treino=treino
@@ -65,3 +85,31 @@ class TreinoVideosModelTest(Geradores_base_mixin,CadastroMixin,TestCase):
         resposta_videos = TreinoVideosmodel.objects.get(usuario=user)
         resposta_videos = list(resposta_videos.videos.all())
         self.assertEqual(resposta_videos,videos)
+
+    def test_treino_videos_model_usario_e_treino_nome_unicos_juntos(self):
+        usuario_1 = self.cadastro_usuario(username='Usuario_1')
+        usuario_2 = self.cadastro_usuario(username='Usuario_2')
+        nome_treino = 'Treino_testando'
+        treino_1 = TreinoModel.objects.create(usuario=usuario_1,treino_nome=nome_treino)
+        treino_2 = TreinoModel.objects.create(usuario=usuario_2,treino_nome=nome_treino)
+
+        TreinoVideosmodel.objects.create(
+            usuario=usuario_1,
+            treino = treino_1,
+        )
+
+        TreinoVideosmodel.objects.create(
+            usuario=usuario_2,
+            treino = treino_2,
+        )
+        quantidade_treinos_videos = TreinoVideosmodel.objects.count()
+        self.assertEqual(quantidade_treinos_videos, 2 )
+
+        with raises(IntegrityError) as error_msg:
+            TreinoVideosmodel.objects.create(
+                usuario=usuario_2,
+                treino = treino_2,
+            )
+        msg_erro = str(error_msg.value)
+        msg_esperada = 'UNIQUE constraint failed:'
+        self.assertIn(msg_esperada, msg_erro)
